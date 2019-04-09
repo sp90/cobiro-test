62bd9fd41f81519f27894e9b4b4c0f483258c634
Simon Dragsbæk Petersen <simon@introdus.dk>
2019-04-09 12:03:15 +0200

    [ADD] dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from './start/start.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/start', pathMatch: 'full' },
   { path: 'start', component: StartComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+
+   // I could make a safeguard here to send people who isn't authed yet sent to login page
+   // But with the time i have, i rather move on with the tasks at hand
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
