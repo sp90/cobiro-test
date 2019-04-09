76cb8aac2b77448f3ebcb23cd82f085f8999685f
Simon Dragsbæk Petersen <simon@introdus.dk>
2019-04-09 12:05:22 +0200

    [ADD] create new customer to auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { StartComponent } from './start/start.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export function tokenGetter() {
+  return localStorage.getItem('access_token')
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     StartComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: [],
+        blacklistedRoutes: []
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 import { CustomerInterface } from '../interfaces/customer';
 
@@ -13,9 +14,16 @@ import { CustomerInterface } from '../interfaces/customer';
 export class AuthService {
   readonly ROOT_URL = 'https://api.test-cobiro.com/api/v1'
 
-  constructor(private http: HttpClient) { }
 
-  newUser(customer: CustomerInterface): Observable<CustomerInterface> {
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('cobiroToken');
+
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+  newCustomer(customer: CustomerInterface): Observable<CustomerInterface> {
     return this.http.post<CustomerInterface>(this.ROOT_URL + '/register', customer)
       .pipe(
         catchError(this.handleError)
